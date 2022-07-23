6bf10174d063dda6a102613cbf8298d2b1372231
Ryan Decker <ryandecker2593@gmail.com>
2022-07-23 12:05:58 -0400

    Added config parameter on wehther to connect to cloud

--- cloud_service/cloud_service.py
+++ cloud_service/cloud_service.py
@@ -13,9 +13,9 @@ import time
 class CloudService(BaseApp):
     def __init__(self, table_name) -> None:
         self.table_name = table_name
-        # self.db = boto3.resource('dynamodb')
-        # self.table = self.db.Table(table_name)
-        # self.client = boto3.client('dynamodb')
+        self.db = None
+        self.table = None
+        self.client = None
         super().__init__("ground.cloud_service")
 
     @property
@@ -50,29 +50,35 @@ class CloudService(BaseApp):
         )
         return response
 
-    def proto_to_cloud(self, msg: proto.Message):
+    def push_to_cloud(self, msg: proto.Message):
         id = msg.telemetry.temperature_data.sensor_id
         temp = msg.telemetry.temperature_data.sensor_value
         now = datetime.datetime.now()
         date=now.strftime('%Y-%m-%d')
         ctime=now.strftime('%H:%M:%S %Z')
-        # self.put(Sensor_Id=str(id), Temperature=str(temp), Date=str(date), Time=str(ctime))
+        if self.config_params.connect_to_database:
+            self.put(Sensor_Id=str(id), Temperature=str(temp), Date=str(date), Time=str(ctime))
         print(f"Uploaded Sample on Cloud Id:{id} T:{temp} D:{date} T:{ctime}")
 
     def setup(self):
-        pass
+        if self.config_params.connect_to_database:
+            self.db = boto3.resource('dynamodb')
+            self.table = self.db.Table(self.table_name)
+            self.client = boto3.client('dynamodb')
+        else:
+            pass
 
     def run(self):
         # Grab the latest telemetry and push to database
         if len(self.telemetry_queue):
             for msg in self.telemetry_queue:
                 if msg.HasField("telemetry"):
-                    self.proto_to_cloud(msg)
+                    self.push_to_cloud(msg)
 
     def shutdown(self):
         pass
 
 
 if __name__ == "__main__":
-    CloudService("FauxTemp")   # Runs the service
+    CloudService("DHT")   # Runs the service
     
